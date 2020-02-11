3306fb27d98ddcd7faab98e70fd4b299de1ce4ad
cesar <cesarfnt@protonmail.com>
2020-02-11 00:28:27 -0300

    segunda alteracao

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello_blog(request):
+    lista = ['Django', 'Python', 'Git', 'HTML', 
+            'Banco de dados', 'Linux', 'nginx', 'Uwsgi'
+            'Systemctl'
+            ]
+    data = {'name': 'Django3', 'lista_tecnologias': lista}
+    return render(request, 'index.html', data)
