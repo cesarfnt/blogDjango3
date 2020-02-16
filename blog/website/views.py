784a47554a0b8d821cb0d084b7a46456e2fd029b
cesar <cesarfnt@protonmail.com>
2020-02-16 17:18:34 -0300

    acrescentado front end do projeto

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
 
 def hello_blog(request):
-    lista = ['Django', 'Python', 'Git', 'HTML', 
+    
+    lista = [
+            'Django', 'Python', 'Git', 'HTML', 
             'Banco de dados', 'Linux', 'nginx', 'Uwsgi'
-            'Systemctl'
+            'Systemctl',
             ]
-    data = {'name': 'Django3', 'lista_tecnologias': lista}
+    list_posts = Post.objects.filter(deleted=False)
+
+
+    data = {
+            'name': 'Django3', 
+            'lista_tecnologias': lista,
+            'posts': list_posts,
+            }
     return render(request, 'index.html', data)
